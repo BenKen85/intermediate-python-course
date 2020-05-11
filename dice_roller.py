67768b4f59b04b73836baf2f7157b83f60aa52d1
Ben Kenifick <kenifickbm@gmail.com>
2020-05-11 20:56:09 +0100

    Tutorial Complete

--- dice_roller.py
+++ dice_roller.py
@@ -1,11 +1,17 @@
 import random
 
 def main():
-  dice_rolls=2
+  dice_rolls= int(input(' How Many dice would you like to Roll? '))
+  dice_size = int(input('How many sides to this muthaflippin dice?'))
   dice_sum=0
   for _ in range (0,dice_rolls):
-    roll = random.randint(1,6)
-    print(f'You rolled a {roll}')
+    roll = random.randint(1,dice_size)
+    if roll ==1:
+      print (f"You rolled a {roll}! Critical Fail")
+    elif roll ==dice_size:
+      print (f"You rolled a {roll}! Great Success!")
+    else:
+      print (f"You rolled a {roll}")
     dice_sum+= roll
   print (f'You rolled a total of {dice_sum}')
 
